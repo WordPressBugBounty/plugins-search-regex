67751ccf5e6829adad9e5b5c9619ef48fc3295d4
Sean Murphy <sean@iamseanmurphy.com>
2024-12-29 12:18:54 +0000

    3.1.2

--- search-regex/build/search-regex-version.php
+++ search-regex/build/search-regex-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'SEARCHREGEX_VERSION', '3.1.1' );
-define( 'SEARCHREGEX_BUILD', 'aaf761e3023b602a77d450f4756d2875' );
+define( 'SEARCHREGEX_VERSION', '3.1.2' );
+define( 'SEARCHREGEX_BUILD', '8a152e6fcd5a8a39d2c814748193eff1' );
 define( 'SEARCHREGEX_MIN_WP', '6.4' );
